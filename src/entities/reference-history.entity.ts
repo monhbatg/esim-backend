c41bccc28ad634fcd603b59c159950b22a9f11fd
Battulga0916 <battulga9192@gmail.com>
2026-01-05 13:00:12 +0800

    admin monitoring

---
+++ src/entities/reference-history.entity.ts
@@ -0,0 +1,45 @@
+
+import { ReferencesModule } from 'src/users/dto/reference-module.enum';
+import { ReferencesType } from 'src/users/dto/reference-type.enum';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('references_history')
+export class ReferencesHistory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  referenceId: string;   // FK but not required to enforce
+
+  @Column({
+    type: 'enum',
+    enum: ReferencesModule,
+  })
+  module: ReferencesModule;
+
+  @Column()
+  key: string;
+
+  @Column({
+    type: 'enum',
+    enum: ReferencesType,
+  })
+  type: ReferencesType;
+
+  @Column({ type: 'text', nullable: true })
+  oldValue: string;
+
+  @Column({ type: 'text', nullable: true })
+  newValue: string;
+
+  @Column({ type: 'text', nullable: true })
+  changedBy: string; // user id / email / service name
+
+  @CreateDateColumn()
+  changedAt: Date;
+}
