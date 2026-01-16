9a2a1b64cabe0d4cc6b933906470a8ec5a281242
Battulga0916 <battulga9192@gmail.com>
2026-01-16 23:21:22 +0800

    backup delete monitoring endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,25 +4,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from '../entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
-import { SettingsReferences } from 'src/entities/references.entity';
-import { ReferencesHistory } from 'src/entities/reference-history.entity';
-import { ESimPurchase } from 'src/entities/esim-purchase.entity';
-import { EsimInvoice } from 'src/entities/esim-invoice.entity';
-import { Customer } from 'src/entities/customer.entity';
-import { Wallet } from 'src/entities/wallet.entity';
-import { Salary } from 'src/entities/salary.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
-    User,
-    //SettingsReferences,
-    ReferencesHistory,
-    ESimPurchase, 
-    EsimInvoice,
-    SettingsReferences,
-    Customer,
-    Wallet,
-    Salary
+    User
   ]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
