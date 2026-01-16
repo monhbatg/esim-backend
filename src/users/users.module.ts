dc4da2a6892e3a1c162786b5f992a3ef009d21a9
Battulga0916 <battulga9192@gmail.com>
2026-01-17 00:54:28 +0800

    backup

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,20 +4,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from '../entities/user.entity';
 import { AuthModule } from '../auth/auth.module';
-import { Customer } from 'src/entities/customer.entity';
-import { EsimInvoice } from 'src/entities/esim-invoice.entity';
-import { ESimPurchase } from 'src/entities/esim-purchase.entity';
-import { ReferencesHistory } from 'src/entities/reference-history.entity';
-import { SettingsReferences } from 'src/entities/settings-references.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
-    User,
-    SettingsReferences,
-    ReferencesHistory,
-    ESimPurchase,
-    EsimInvoice,
-    Customer,
+    User
   ]), forwardRef(() => AuthModule)],
   controllers: [UsersController],
   providers: [UsersService],
